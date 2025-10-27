5364f071f6cfa6aa8dbfb4a03a4a9495af0c4db6
bykimberly <kimberlychau04@gmail.com>
2025-10-26 20:29:58 -0700

    testing allminiLM

---
+++ TransformerNLP/search.py
@@ -0,0 +1,31 @@
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+# Example data
+sentences = [
+    "The restaurant has a wheelchair ramp.",
+    "There is a ramp for wheelchairs.",
+    "The cafe is not accessible because of stairs.",
+    "The food was delicious!"
+]
+
+# TODO: next step is to encode restaurant reviews from our larger dataset
+embeddings = model.encode(sentences)
+
+print("Embeddings shape:", embeddings.shape)
+
+# Find which sentence is most similar to user query
+# query = "Does the place have wheelchair access?"
+query = "Is there a ramp and accessible restroom?"
+query_emb = model.encode([query])
+
+scores = cosine_similarity(query_emb, embeddings)[0]
+best_idx = np.argmax(scores)
+
+print("\nQuery:", query)
+print("Most similar sentence:", sentences[best_idx])
+print("Similarity score:", scores[best_idx])
