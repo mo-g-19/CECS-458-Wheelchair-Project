217586adaf8143517049ed65fd5c5de13be25394
mo-g-19 <196864565+mo-g-19@users.noreply.github.com>
2025-11-13 09:56:42 -0800

    Forgot the comments

--- src/GNNs/common/__init__.py
+++ src/GNNs/common/__init__.py
@@ -1,3 +1,7 @@
-#This makes the common folder immportable
+#This makes the common folder immportable (a namespace package)
+#   Reviewed the difference between a package and a namespace package today!
+#       regular package: has __init__.py, and file is implicity executed and the object is defined to bound names in package
+#           I knew calling __init__ as a function initialized an object, but didn't realize it also bound names in the package namespace
+#       namespace package: no __init__.py, and files are not executed until explicitly imported
 #optional re-exports
 from .data_sources import DataSource, LocalCSV
